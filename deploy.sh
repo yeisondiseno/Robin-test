d1087eede20a434701757b18c4382b6c27e37121
yeisondiseno <yeisondiseno@gmail.com>
2022-05-11 15:04:24 -0500

    Y.Montoya; save

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,5 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
-git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
 cd -
